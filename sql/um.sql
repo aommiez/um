7612edab79cce2daac055629b06344af6b650202
MRG <aommiez@gmail.com>
2015-06-08 15:00:20 +0700

    Update um.sql

--- sql/um.sql
+++ sql/um.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.3
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Jun 03, 2015 at 12:04 PM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Host: 127.0.0.1
+-- Generation Time: Jun 07, 2015 at 05:08 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,20 +27,45 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `employee` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `first_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `emp_type` int(11) NOT NULL,
-  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `blacklist` int(11) NOT NULL DEFAULT '0',
+  `visa_expiry` date DEFAULT NULL,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `employee`
 --
 
-INSERT INTO `employee` (`id`, `user_id`, `first_name`, `last_name`, `emp_type`, `create_date`) VALUES
-(1, 1, 'กกกก', 'ขขข', 2, '2015-06-03 09:28:34');
+INSERT INTO `employee` (`id`, `user_id`, `first_name`, `last_name`, `blacklist`, `visa_expiry`, `update_time`) VALUES
+(1, 1, 'กกกก', 'ขขข', 0, '2015-06-08', '2015-06-07 11:09:29'),
+(2, 2, 'teassasa', 'asdsadas', 1, '2015-06-29', '2015-06-07 11:09:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employer`
+--
+
+CREATE TABLE IF NOT EXISTS `employer` (
+`id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `blacklist` int(11) NOT NULL DEFAULT '0',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `employer`
+--
+
+INSERT INTO `employer` (`id`, `user_id`, `first_name`, `last_name`, `blacklist`, `update_time`) VALUES
+(1, 2, 'jjjjj', 'jjjj', 1, '2015-06-07 11:09:53'),
+(2, 2, 'kkk', 'kkk', 0, '2015-06-07 11:09:53');
 
 -- --------------------------------------------------------
 
@@ -49,7 +74,7 @@ INSERT INTO `employee` (`id`, `user_id`, `first_name`, `last_name`, `emp_type`, 
 --
 
 CREATE TABLE IF NOT EXISTS `emp_type` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -83,11 +108,44 @@ INSERT INTO `level` (`id`, `level_name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `nationality`
+--
+
+CREATE TABLE IF NOT EXISTS `nationality` (
+`id` int(11) NOT NULL,
+  `nationality_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `race`
+--
+
+CREATE TABLE IF NOT EXISTS `race` (
+`id` int(11) NOT NULL,
+  `race_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `religious`
+--
+
+CREATE TABLE IF NOT EXISTS `religious` (
+`id` int(11) NOT NULL,
+  `religious_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -114,19 +172,43 @@ INSERT INTO `user` (`id`, `username`, `password`, `email`, `level`, `first_name`
 -- Indexes for table `employee`
 --
 ALTER TABLE `employee`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employer`
+--
+ALTER TABLE `employer`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `emp_type`
 --
 ALTER TABLE `emp_type`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `nationality`
+--
+ALTER TABLE `nationality`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `race`
+--
+ALTER TABLE `race`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `religious`
+--
+ALTER TABLE `religious`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -136,17 +218,37 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `employer`
+--
+ALTER TABLE `employer`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `emp_type`
 --
 ALTER TABLE `emp_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `nationality`
+--
+ALTER TABLE `nationality`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `race`
+--
+ALTER TABLE `race`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `religious`
+--
+ALTER TABLE `religious`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
